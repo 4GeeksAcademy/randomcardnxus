d341a88b271b4c8008111cc81e3411c1b0fde00b
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-07-26 17:06:43 +0000

    cambio final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio];
+  }
+
+  let palo = ["♦", "♥", "♠", "♣"];
+  let numero = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let paloCarta = aleatorio(palo);
+  let elementPaloCarta = document.querySelector("#paloCarta");
+  elementPaloCarta.innerHTML = paloCarta;
+
+  let numCarta = aleatorio(numero);
+  let elementnumCarta = document.querySelector("#numCarta");
+  elementnumCarta.innerHTML = numCarta;
+
+  let elementPaloCartaAbajo = document.querySelector("#paloCartaAbajo");
+  elementPaloCartaAbajo.innerHTML = paloCarta;
+
+  elementPaloCartaAbajo.style.transform = "rotate(180deg)";
+  if (paloCarta == "♥" || paloCarta == "♦") {
+    elementPaloCartaAbajo.style.color = "red";
+    elementPaloCarta.style.color = "red";
+  }
 };
